10d7193bd5a4a45cfb3296d7ed064ece5569a48b
dwifahriza <max@MacBook-Air-Max.local>
2022-02-03 11:39:24 +0700

    first commit

---
+++ Assets/Low_Swordman/Demo/Scripts/CameraControlllor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlllor : MonoBehaviour
+{
+    //Kamera Ruangan
+    [SerializeField] private float speed;
+    private float currentPostX;
+    private Vector3 velocity = Vector3.zero;
+
+    //Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    private void Update()
+    {
+        //Kamera Ruangan
+        ///transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPostX, transform.position.y, transform.position.z),ref velocity, speed );
+
+        //Follow Player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPostX = _newRoom.position.x;
+    }
+}
---
+++ Assets/Script/Life/Health.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    private float timer = 10f;
+    private bool setTimer = false;
+    private bool setriger = false;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            //Player Hurt
+            anim.SetTrigger("hurt");
+        } else
+        {
+            if(!dead)
+            {
+               isDead();
+            }
+           
+        }
+    }
+
+    // belum terpakai
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private void isDead(){
+
+        Debug.Log(timer);
+        if (setTimer == true)
+        {
+            anim.SetTrigger("die");
+            timer -= Time.deltaTime * 15;
+            if(timer < 1f)
+            {
+                setriger = true;
+                
+            }
+        }
+
+        if (setriger == true)
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            dead = true;
+            SceneManager.LoadScene(0);
+
+        }
+    }
+
+    private void Update(){
+        isDead();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "enemy")
+        {
+            setTimer = true;
+        }
+    }
+}
